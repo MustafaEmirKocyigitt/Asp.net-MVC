1098a2413a1277c0ff37c6b22bec809ffab723b0
Mustafa Emir Koçyiğit <mustafaemirkocyigit@gmail.com>
2023-12-14 14:14:06 +0300

    Asp.net İle Ürün Ekleme Silme Güncelleme ve Müşteri Ekleme Silme Güncelleme

---
+++ Proje_OOP/Proje_OOP/Entity/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Proje_OOP.Entity
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Proje_OOP/Proje_OOP/ProjeContext/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proje_OOP.Entity;
+
+namespace Proje_OOP.ProjeContext
+{
+    public class Context : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MUSTA;database=DbNewOppCore;integrated security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
